4a941191cdb30f81d96b8f7c812ce265e93f27ff
Parktaesunkr <spcsm5452@naver.com>
2023-05-31 17:41:11 +0900

    loginJSP연습

---
+++ pro09/src/main/java/sec05/ex01/LoginServlet.java
@@ -0,0 +1,60 @@
+package sec05.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doHandle(request, response);
+		}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String user_id = request.getParameter("user_id");
+		String user_pwd = request.getParameter("user_pwd");
+		MemberVO memberVO = new MemberVO();
+		memberVO.setId(user_id);
+		memberVO.setPwd(user_pwd);
+		MemberDAO dao = new MemberDAO();
+		boolean result = dao.isExisted(memberVO);
+		if(result) {
+			HttpSession session = request.getSession();
+			session.setAttribute("isLogon", true);
+			session.setAttribute("login.id", user_id);
+			session.setAttribute("login.pwd", user_pwd);
+			out.println("<html><body>");
+			out.println("안녕하세요" + user_id+"님!!<br>");
+			out.println("<a href='show'>회원정보 보기</a>");			
+			out.println("</body></html>");
+		}else {
+			out.println("<html><body><center>회원 아이디가 틀립니다.");
+			out.println("<a href='login3.html'>다시 로그인하기</a>");			
+			out.println("</body></html>");
+			
+		}
+		
+	}
+
+}
